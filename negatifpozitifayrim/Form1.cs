20e34d8a69461e0380b514875b0f42acb50ff90a
Ahmet Yeşeriroğlu <ahmetgs131@gmail.com>
2022-12-03 13:03:31 +0300

    Proje dosyası ekle.

---
+++ negatifpozitifayrim/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace negatifpozitifayrim
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int i, sayi;
+            for (i = 0; i < 10; i++)
+            {
+                sayi = r.Next(-100, 100);
+                listBox1.Items.Add(sayi);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            int i = 0;
+            for (i = 0; i < 10; i++)
+            {
+                textBox1.Text = listBox1.Items[i].ToString();
+                if (Convert.ToInt16(textBox1.Text) < 0)
+                {
+                    listBox2.Items.Add(listBox1.Items[i]);
+                }
+                else
+                {
+                    listBox3.Items.Add(listBox1.Items[i]);
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
